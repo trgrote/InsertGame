8f0c953c382e003086afa42604d7b2b2755448d2
Taylor Grote <grotese@gmail.com>
2017-06-10 13:53:34 -0500

    Add death to people

---
+++ Assets/Scripts/People/PeopleDeath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// On Death Replace yourself with a splatter
+public class PeopleDeath : MonoBehaviour
+{
+	[SerializeField]
+	GameObject deadPerson;
+
+	void OnAliveChanged(bool alive)
+	{
+		Instantiate(
+			deadPerson,
+			transform.position,
+			transform.rotation
+		);
+
+		// Do what all advertisers should do
+		Destroy(gameObject);   // kill self
+	}
+
+	void Update()
+	{
+		// Test
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<PeopleState>().Alive = false;
+		}
+	}
+}
---
+++ Assets/Scripts/People/PeopleState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// State of people
+public class PeopleState : MonoBehaviour 
+{
+	private bool _alive = true;
+	public bool Alive
+	{
+		get
+		{
+			return _alive;
+		}
+		set
+		{
+			_alive = value;
+			BroadcastMessage("OnAliveChanged", _alive);
+		}
+	}
+}
