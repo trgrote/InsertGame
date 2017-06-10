16d3ae2ac779df315ee00938ee3524880b9de48a
Taylor Grote <grotese@gmail.com>
2017-06-10 14:40:44 -0500

    Add random flip for dead sprites

---
+++ Assets/Scripts/People/RandomFlip.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class RandomFlip : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<SpriteRenderer>().flipX = Random.value < 0.5;
+	}
+}
