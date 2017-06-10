ea0576bb7bfcb1f4d217868dd9691b69854b85f8
Taylor Grote <grotese@gmail.com>
2017-06-10 12:57:59 -0500

    Add Y to Sorting layer fix

---
+++ Assets/Scripts/People/YtoSortingOrder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class YtoSortingOrder : MonoBehaviour
+{
+	SpriteRenderer sprite;
+
+	void Start()
+	{
+		sprite = GetComponent<SpriteRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		int pos = Mathf.RoundToInt(transform.position.y);
+		pos /= 3;
+		sprite.sortingOrder = -pos;
+	}
+}
