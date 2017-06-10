f7b04d2978b5c1b49467aec69d450d632a3c59c8
Taylor Grote <grotese@gmail.com>
2017-06-10 16:26:52 -0500

    Add Random death scream to dead persons

---
+++ Assets/Scripts/People/RandomDeathScream.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RandomDeathScream : MonoBehaviour
+{
+	// Chance to play any sound
+	[SerializeField, Range(0,1)] float chanceToPlay = 0.2f;
+	[SerializeField] AudioClip[] clips;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (Random.value < chanceToPlay)
+		{
+			var audio = GetComponent<AudioSource>();
+			audio.clip = clips[
+				Random.Range(0, clips.Length)
+			];
+			audio.Play();
+		}
+	}
+}
