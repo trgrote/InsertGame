fe185a86995f86bc8ebc824c292667bfa03b2814
Taylor Grote <grotese@gmail.com>
2017-06-10 11:54:00 -0500

    Add Randomized Sprite for People

---
+++ Assets/Scripts/People/PeopleRandomSprite.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Choose Random Sprite for people
+[RequireComponent(typeof(SpriteRenderer))]
+public class PeopleRandomSprite : MonoBehaviour
+{
+	[SerializeField]
+	Sprite[] possibleSprites;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<SpriteRenderer>().sprite = possibleSprites[Random.Range(0, possibleSprites.Length)];
+	}
+}
