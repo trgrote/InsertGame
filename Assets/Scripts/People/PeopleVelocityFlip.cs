d6cdb043fa65f3227058e327d1260ebbeb70de11
Taylor Grote <grotese@gmail.com>
2017-06-10 12:11:28 -0500

    Add Sprite flipping to people

---
+++ Assets/Scripts/People/PeopleVelocityFlip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Flip the X of the Sprite based off current x velocity
+[RequireComponent(typeof(SpriteRenderer), typeof(Rigidbody2D))]
+public class PeopleVelocityFlip : MonoBehaviour
+{
+	SpriteRenderer sprite;
+	Rigidbody2D body;
+
+	void Start()
+	{
+		sprite = GetComponent<SpriteRenderer>();
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Mathf.Approximately(body.velocity.x, 0))
+			sprite.flipX = body.velocity.x < 0;
+	}
+}
