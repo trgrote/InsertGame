e1ccd16c0dabac679c1ee92d860bfa04102c460a
Taylor Grote <grotese@gmail.com>
2017-06-11 14:41:03 -0500

    Add Edit Sorting layer

---
+++ Assets/Scripts/EditorSortingLayerFix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Renderer))]
+public class EditorSortingLayerFix : MonoBehaviour 
+{
+	public string sortingLayerName = "Players";
+	public int sortingOrder = 0;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetComponent<Renderer>().sortingLayerName = sortingLayerName;
+		GetComponent<Renderer>().sortingOrder = sortingOrder;
+	}
+}
